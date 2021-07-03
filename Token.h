22e4ae29b99485e27d2e0c7699f391818b443b48
Andres Matos <andresmatos2000@gmail.com>
2021-07-03 09:00:02 -0400

    Copy of project1

---
+++ Token.h
@@ -0,0 +1,40 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+#include <string>
+#include <iostream>
+
+enum class TokenType {
+    COMMA,
+    PERIOD,
+    Q_MARK,
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    COLON,
+    COLON_DASH,
+    MULTIPLY,
+    ADD,
+    SCHEMES,
+    FACTS,
+    RULES,
+    QUERIES,
+    ID,
+    STRING,
+    COMMENT,
+    UNDEFINED,
+    TYPE_EOF
+};
+
+class Token {
+private:
+    std::string _description;
+    int line_number;
+    TokenType tokenType;
+
+public:
+    Token(TokenType type, std::string description, int line);
+
+    std::string tokenTypeToString(TokenType inputType);
+    std::string To_String();
+};
+#endif // TOKEN_H
+
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include "Lexer.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+std::string readCodeIn(char** argv){
+    //cout << argv[1];
+    ifstream in;
+    in.open(argv[1]);
+    std::string fullString;
+    std::string line;
+    while(getline(in,line)) {
+        fullString += line + "\n";
+    }
+    in.close();
+    return fullString;
+}
+
+int main(int argc, char** argv) {
+
+    std::string fullString = readCodeIn(argv);
+    Lexer* lexer = new Lexer();
+    lexer->Run(fullString);
+    std::vector<Token*> tokenList = lexer->getTokens();
+    for(unsigned int i = 0; i < tokenList.size(); i++){
+        cout << tokenList[i]->To_String() << endl;
+    }
+    cout << "Total Tokens = " + to_string(tokenList.size());
+    delete lexer;
+
+    return 0;
+}
+
